2c39e99ad3cfb35929ba28fe24d583de47b507ee
Nikita <guschinnick@gmail.com>
2020-01-06 19:27:30 +0300

    01

--- lesson03/DZ03.java
+++ lesson03/DZ03.java
@@ -1,7 +1,28 @@
 package ru.geekbrains.java02.lesson03;
 
+import java.util.*;
+
 public class DZ03 {
     public static void main(String[] args){
-        
+        ArrayList<String> fruits=new ArrayList<>(Arrays.asList(
+                "яблоко","банан","апельсин","персик","мандарин","киви","апельсин","персик","ананас","лимон","киви","персик","апельсин","киви","апельсин"
+        ));
+        System.out.println(getUniq(fruits));
+        System.out.println(countArr(fruits));
+    }
+
+    private static ArrayList<String> getUniq(ArrayList<String> arr){
+        ArrayList<String> out=new ArrayList<>();
+        for (String o: arr) {
+            if(Collections.frequency(arr,o)==1)out.add(o);
+        }
+        return out;
+    }
+    private static HashMap<String,Integer> countArr(ArrayList<String> arr){
+        HashMap<String,Integer> out=new HashMap<>();
+        for (String o: arr) {
+            out.put(o,Collections.frequency(arr,o));
+        }
+        return out;
     }
 }
